8622b1fbd68f655f7e0b568d9a1d4e19d21c1e1c
leandro.rocha <leandro.rocha@redelojacorr.com.br>
2022-08-25 19:04:22 -0300

    add

--- Dia_1/Exercicio_print.py
+++ Dia_1/Exercicio_print.py
@@ -1,5 +1,26 @@
-a = input("What is your name: ")
-b = input("Your age: ")
-print("Nome: ", a + "\nIdade: ", b)
- 
+## Print com input e quebrando linha
+# a = input("What is your name: ")
+# b = input("Your age: ")
+# print("Nome: ", a + "\nIdade: ", b)
+# print(len(input ("Qual o seu nome: ")))
 
+## Contando o número de letras
+
+# a = input("What is your name: ")
+# b = input("Your age: ")
+# print("Nome: ", a + "\nIdade: ", b)
+# print (len(a+b))
+
+print ("Welcome to the Band Name Generation.")
+city_name = (input ("What's name of the city you grew up in? "))
+print (city_name)
+pet_name = (input ("What's your pet's name "))
+print (pet_name)
+print ("Your band name could be",city_name, pet_name)
+
+
+
+
+
+ 
+ 
