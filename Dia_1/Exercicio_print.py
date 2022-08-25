2da393a9b9b56793997cb5a87525fc223a10790c
leandro.rocha <leandro.rocha@redelojacorr.com.br>
2022-08-25 19:10:26 -0300

    add

--- Dia_1/Exercicio_print.py
+++ Dia_1/Exercicio_print.py
@@ -11,12 +11,13 @@
 # print("Nome: ", a + "\nIdade: ", b)
 # print (len(a+b))
 
-print ("Welcome to the Band Name Generation.")
-city_name = (input ("What's name of the city you grew up in? "))
-print (city_name)
-pet_name = (input ("What's your pet's name "))
-print (pet_name)
-print ("Your band name could be",city_name, pet_name)
+# # Projeto final da aula 1
+# print ("Welcome to the Band Name Generation.")
+# city_name = (input ("What's name of the city you grew up in? "))
+# print (city_name)
+# pet_name = (input ("What's your pet's name "))
+# print (pet_name)
+# print ("Your band name could be",city_name, pet_name)
 
 
 
