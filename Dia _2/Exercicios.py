7fba5d575754d95a58cbcdd5909ebb985e6e7dfb
leandro.rocha <leandro.rocha@redelojacorr.com.br>
2022-08-26 16:12:04 -0300

    add

--- Dia _2/Exercicios.py
+++ Dia _2/Exercicios.py
@@ -1,4 +1,20 @@
 # Exercicio 1
-two_digit_number = input("Type a two digit number: ")
+#two_digit_number = input("Type a two digit number: ")
 
-print (int(two_digit_number[0]) + int(two_digit_number[1]))
+#print (int(two_digit_number[0]) + int(two_digit_number[1]))
+
+## Operações matemáticas seguem uma prioridade, () ** * / + -, abaixo vai resolver primeiro a *, depois / e por último a -
+#print (int(3 * 3 + 3 / 3 - 3))
+
+## Abaixo eu forço a prioridade para a adição, depois a divisão para poder ter o resultado q quero, q é 3.
+#print(3 * (3 + 3) / 3 - 3)
+
+##Exercicio calculadora de IMC
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+p = int(weight)
+a = float(height)
+soma = p / a ** 2
+print (weight, "%", height, "x", height, "=", soma,"\n",int(soma) )
+
