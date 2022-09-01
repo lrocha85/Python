3abd4720dd2106b507f70d150f55615d36b7118d
leandro.rocha <leandro.rocha@redelojacorr.com.br>
2022-09-01 18:19:54 -0300

    add

--- Dia_7/main.py
+++ Dia_7/main.py
@@ -1,15 +1,110 @@
-# Challenge 1
+# # Challenge 1
+# import random
+
+# word_list = ["ardvark", "baboon", "camel"]
+# chosen_word = random.choice(word_list)
+
+# guess = input("What is the letter? ").lower()
+
+# for word in chosen_word:
+#     if word == guess:
+#         print("Right")
+#     else:
+#         print("Wrong")
+
+# Challenge 2
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+print (stages[6])
+
+end_of_game = False
 word_list = ["ardvark", "baboon", "camel"]
-
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+life = 6
 
-guess = input("What is the letter? ").lower()
+display = []
 
-for word in chosen_word:
-    if word == guess:
-        print("Right")
-    else:
-        print("Wrong")
+for length in range(word_length):
+    display += "_"
 
+print (display)
+
+while not end_of_game:
+    guess = input("What is the letter? ").lower()
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+            print (life)
+        
+        if guess not in chosen_word:
+            life -= 1
+            if life == 0:
+                end_of_game = True
+                print (f"You lose! Life:{life}")   
+
+        if "_" not in display:
+            end_of_game = True
+            print ("You win!")
+    print (f"stages{life}\n{display}")
+
+    
