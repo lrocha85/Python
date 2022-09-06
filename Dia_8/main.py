1880d9d064412a8e988f233438e3ed00fa8503cc
leandro.rocha <leandro.rocha@redelojacorr.com.br>
2022-09-06 19:10:51 -0300

    add

--- Dia_8/main.py
+++ Dia_8/main.py
@@ -12,15 +12,92 @@
 # paint_calc(height=test_h, width=test_w, cover=coverage)
 
 #Exercise 2 - Prime Numbers:
-def prime_checker(number):
-    if number > 1: # Número 1 nunca é impar
-        for i in range(2, number): # Vai testar se o número escolhido é totalmente divisível por 2-até o número
-# anterior a ele mesmo, exemplo, se o número escolhido for 6, vai testar 6/2 6/3 6/4 e 6/5, se for totalmente divisível por alguma, não é número primo            
-            if (number % i) == 0:
-                return print ("It's not a prime number")                
-        return print ("It's a prime number.")
-    else:
-        return print ("It's not a prime number.")
+# def prime_checker(number):
+#     if number > 1: # Número 1 nunca é impar
+#         for i in range(2, number): # Vai testar se o número escolhido é totalmente divisível por 2-até o número
+# # anterior a ele mesmo, exemplo, se o número escolhido for 6, vai testar 6/2 6/3 6/4 e 6/5, se for totalmente divisível por alguma, não é número primo            
+#             if (number % i) == 0:
+#                 return print ("It's not a prime number")                
+#         return print ("It's a prime number.")
+#     else:
+#         return print ("It's not a prime number.")
 
-n = int(input("Check this number: "))
-prime_checker(number=n)
+# n = int(input("Check this number: "))
+# prime_checker(number=n)
+
+# fruits = [4, 55, 64, 32, 16, 32]
+
+# x = fruits.index(32) # Função index mostra a posição do item passado na lista
+
+# print (x)
+
+## caesar-cipher-1-start
+
+# from operator import index
+# from tkinter import N
+
+
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
+
+
+
+# def encrypt(text, shift):
+#     final_text = ""
+#     for i in (text):
+#         word = alphabet.index(i)
+#         word_encode = word + shift
+#         final_text += alphabet[word_encode]
+#     print(f"The encoded text is {final_text}")
+
+# def decrypt(text, shift):
+#     final_text = ""
+#     for i in (text):
+#         word = alphabet.index(i)
+#         word_encode = word - shift
+#         final_text += alphabet[word_encode]
+#     print(f"The encoded text is {final_text}")
+
+# if direction == "encode":
+#     encrypt(text, shift)
+# else:
+#     decrypt(text, shift)
+
+## Juntando as 2 funções acima em 1 só chamada cesar:
+
+from operator import index
+from art import logo
+
+print (logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+reload = ""
+while reload == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    def cesar(text, shift, direction):
+        final_text = ""
+        if direction == "decode":
+            shift *= -1
+        for i in text:
+            if i in alphabet:
+                word = alphabet.index(i)
+                new_word = word + shift
+                final_text += alphabet[new_word]
+            else:
+                final_text += i
+
+        print (f"Here's the {direction}d result: {final_text} ")
+        reload= input("Type 'yes' if you want to go again. Otherwise type 'no': ")
+            
+    cesar(text, shift, direction)   
+
+print ("Goodbye")
